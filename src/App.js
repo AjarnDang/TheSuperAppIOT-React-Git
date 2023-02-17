a4262793d94ea0b4ee53bb4050b9d018d8e4192d
AjarnDang <torntan.j@gmail.com>
2023-02-17 13:40:04 +0700

    New Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './pages/Login'
 import Index from './pages/Index';
 import Register from './pages/Register';
+
+// CSS
+import './assets/css/all.min.css'
+import './assets/css/sb-admin-2.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
@@ -13,7 +16,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/Index" element={<Index />} />
         <Route path="/Register" element={<Register />} />
-      </Routes>
+      </Routes>   
     </BrowserRouter>
     </div>  
   );
--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div>
+      <nav className='bg-dark'>
+      Header
+      </nav>   
+    </div>
   )
 }
 
